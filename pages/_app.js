3eb1fe70b0284c55f73e432a0136ce6d56a9c29c
Mir Misbah Ali <mirmisbahali@hotmail.com>
2022-08-29 13:59:10 +0530

    created sidebar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import '../public/assets/css/sb-admin-2.min.css'
+import Layout from '../Components/Layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+  )
 }
 
 export default MyApp
